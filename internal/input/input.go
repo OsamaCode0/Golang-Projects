492d056a80c9d30596f22a843cfed22b24c13856
valte0 <valiahde.v@gmail.com>
2025-05-06 00:05:59 +0300

    Additional error handling

--- internal/input/input.go
+++ internal/input/input.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 // Processes user's input and handles errors
@@ -15,14 +16,23 @@ func ProcessInput() (*bufio.Scanner, *os.File){
 		os.Exit(1)
 	}
 
+	
+
 	// Save arguments to variables
 	networkPath := os.Args[1]
 	startStation := os.Args[2]
 	endStation := os.Args[3]
 	numTrainsStr := os.Args[4] 
 
-	// Check that value has been given to each argument
-	if networkPath== ""{
+	numTrainInt, err := strconv.Atoi(numTrainsStr)
+
+	if err != nil{
+		return 
+	}
+
+
+	// Error handling
+	if networkPath == ""{
 		fmt.Fprintln(os.Stderr, "Error: Please enter path to the network map file")
 		os.Exit(1)
 	}
@@ -37,6 +47,14 @@ func ProcessInput() (*bufio.Scanner, *os.File){
 	}
 	if numTrainsStr == ""{
 		fmt.Fprintln(os.Stderr, "Error: Please enter the amount of trains")
+		os.Exit(1)
+	}
+	if numTrainInt < 1{
+		fmt.Fprintln(os.Stderr, "Error: Number of trains has to be a positive integer")
+	}
+
+	if startStation == endStation{
+		fmt.Fprintln(os.Stderr, "Error: Cannot have the same starting and ending station")
 		os.Exit(1)
 	}
 
@@ -58,6 +76,16 @@ func ProcessInput() (*bufio.Scanner, *os.File){
 
 
 	return scanner, networkMap
+
+}
+
+
+
+// Handles the logic of trains
+func ProcessTrains(){
+
+
+
 
 }
 
