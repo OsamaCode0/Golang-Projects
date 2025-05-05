b0242672ef38eae1aef5da09e0ce657d072b07bb
valte0 <valiahde.v@gmail.com>
2025-05-06 00:55:37 +0300

    Returns stations and connections to be fed to the pathfinding algorithm, also connects two stations together. Doesn't handle coordinates or all of the error cases yet.

--- internal/parser/parsefile.go
+++ internal/parser/parsefile.go
@@ -2,91 +2,86 @@ package parser
 
 import (
 	"fmt"
+	//"os"
 	"stations/internal/input"
 	"strings"
-	
 )
 
-
-type Connection struct{
-
+type Connection struct {
 	From string
-	To string
-
+	To   string
 }
-
-
 
 // Parses the network map
-func ParseMap() ([]string, []Connection, error){
+func ParseMap() ([]string, []Connection, error) {
 
-		// Get the scanned file from the input processing function
-		scanner, file := input.ProcessInput()
-		defer file.Close()
+	// Get the scanned file from the input processing function
+	scanner, file := input.ProcessInput()
+	defer file.Close()
 
-		var stations []string
-		var connections []Connection
-		inStations := false
-		inConnections := false
-	
-		for scanner.Scan() {
-			
-	
-			line := strings.TrimSpace(scanner.Text())
-		
-			if line == "" {
-				continue
-			}
-			if strings.HasPrefix(line, "#") {
-				continue
-			}
+	var stations []string
+	var connections []Connection
+	inStations := false
+	inConnections := false
 
-			// Mark sections
-			switch {
-			case line == "stations:":
-				inStations = true
-				inConnections = false
-				continue
-			case line == "connections:":
-				inStations = false
-				inConnections = true
-				continue
-			}
-	
-			if inStations {
-				if line != "" {
-					stations = append(stations, line)
-				}
-			} else if inConnections {
-				if line != "" {
-					parts := strings.Split(line, "-")
+	for scanner.Scan() {
 
-					if len(parts) != 2 {
-					return nil, nil, fmt.Errorf("invalid connection line: %q", line)
-					}
+		line := strings.TrimSpace(scanner.Text())
 
-					from := strings.TrimSpace(parts[0])
-					to := strings.TrimSpace(parts[1])
-
-					if from == "" || to == "" {
-						return nil, nil, fmt.Errorf("invalid connection endpoints: %q", line)
-					}
-
-					connections = append(connections, Connection{From: from, To: to})
-				}
-					
-				}
-			}
-	
-		if err := scanner.Err(); err != nil {
-			fmt.Println("Error while parsing:", err)
+		if line == "" {
+			continue
 		}
-	
-		// Debug prints
-		fmt.Println("Stations:", stations)
-		fmt.Println("Connections:", connections)
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
 
+		// Mark sections
+		switch {
+		case line == "stations:":
+			inStations = true
+			inConnections = false
+			continue
+		case line == "connections:":
+			inStations = false
+			inConnections = true
+			continue
+		}
+
+		if inStations {
+			if line != "" {
+				stations = append(stations, line)
+			}
+		} else if inConnections {
+			if line != "" {
+				parts := strings.Split(line, "-")
+
+				if len(parts) != 2 {
+					return nil, nil, fmt.Errorf("Error: Invalid connection line: %q", line)
+				}
+
+				from := strings.TrimSpace(parts[0])
+				to := strings.TrimSpace(parts[1])
+
+				if from == "" || to == "" {
+					return nil, nil, fmt.Errorf("Error: Invalid connection endpoints: %q", line)
+				}
+
+				connections = append(connections, Connection{From: from, To: to})
+			}
+
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error while parsing:", err)
+	}
+
+	// Debug prints
+	fmt.Println("Stations:", stations)
+	fmt.Println("Connections:", connections)
+	fmt.Println("number of trains:", input.NumTrainsInt)
 
 	return stations, connections, nil
-}
 
+
+}
