8c5ee1145dab056fe0169106f2725fa257b0bfad
valte0 <valiahde.v@gmail.com>
2025-05-05 17:07:01 +0300

    Removed input handling function from main

--- cmd/main.go
+++ cmd/main.go
@@ -3,81 +3,21 @@ package main
 
 import(
 
-	"fmt"
-	"os"
-	"io"
+	
+	"stations/internal/input"
+	"stations/internal/parser"
+	
 
 )
-
-
-// Processes input and handles errors
-func ProcessInput()([]byte, error){
-
-	// Check that at least 4 arguments are given + name
-	if len(os.Args) < 5{
-		fmt.Fprintln(os.Stderr, "Error: Too few command line arguments")
-		os.Exit(1)
-	}
-
-
-	// Save arguments to variables
-	networkPath := os.Args[1]
-	startStation := os.Args[2]
-	endStation := os.Args[3]
-	numTrainsStr := os.Args[4] 
-
-	// Check that value has been given to each argument
-	if networkPath== ""{
-		fmt.Fprintln(os.Stderr, "Error: Please enter path to network map file")
-		os.Exit(1)
-	}
-	if startStation == ""{
-		fmt.Fprintln(os.Stderr, "Error: Please specify the starting station")
-		os.Exit(1)
-	}
-
-	if endStation == ""{
-		fmt.Fprintln(os.Stderr, "Error: Please specify the end station")
-		os.Exit(1)
-	}
-	if numTrainsStr == ""{
-		fmt.Fprintln(os.Stderr, "Error: Please enter the amount of trains")
-		os.Exit(1)
-	}
-
-
-	// Open networkmap for reading
-	networkMap, err := os.Open(networkPath)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error: Network map not found!")
-		os.Exit(1)
-	}
-	defer networkMap.Close()
-
-
-	// Read the entire file content
-	content, err := io.ReadAll(networkMap)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading network map:", err)
-		os.Exit(1)
-	}
-
-
-
-	// Debug print
-	fmt.Println(string(content))
-
-
-	return content, nil
-
-}
 
 
 
 
 func main(){
 
-	ProcessInput()
+	input.ProcessInput()
+	parser.ParseMap()
+
 
 
 }
